f81d00b7dc4112ea148d49fb45f4aad7384ab078
Jwala <imjwala@users.noreply.github.com>
2025-06-13 14:07:17 +0545

    buildemy backend

---
+++ src/route/institute/instituteRoute.ts
@@ -0,0 +1,9 @@
+import express, {Router} from 'express'
+import InstituteController from '../../controller/institute/instituteController'
+
+ 
+const router:Router = express.Router()
+router.route("/").post(InstituteController.createInstitute)
+
+
+ export default router
