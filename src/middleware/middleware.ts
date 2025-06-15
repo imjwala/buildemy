9dfcfe48b990f80d9f781e5264667b14523e88c2
Jwala <imjwala@users.noreply.github.com>
2025-06-15 13:52:36 +0545

    small bug fixed inside middleware.ts

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -6,38 +6,38 @@ import { IExtendedRequest } from "./type";
 
 
 
-class Middleware{
+class Middleware {
 
-  static  isLoggedIn(req:IExtendedRequest,res:Response, next:NextFunction){
+  static isLoggedIn(req: IExtendedRequest, res: Response, next: NextFunction) {
     //check if login or not
     //token accept
     const token = req.headers.authorization
-    if(!token){
+    if (!token) {
       res.status(403).json({
-        msg:"please provide token"
+        msg: "please provide token"
       })
       return
     }
     //verify token
-    jwt.verify(token,'thisissecret',async (error,result:any)=>{
-      if(error){
+    jwt.verify(token, 'thisissecret', async (error, result: any) => {
+      if (error) {
         res.status(401).json({
-          msg:"Token invalid"
-        })   
-      }else{
+          msg: "Token invalid"
+        })
+      } else {
         const userData = await User.findByPk(result.id)
-        if(!userData){
+        if (!userData) {
           res.status(403).json({
-            msg:"No user with that id, invalid token"
+            msg: "No user with that id, invalid token"
           })
-        }else{
+        } else {
           req.user = userData
           next()
-       
+
         }
       }
     })
-    next()
+
 
   }
   // static restrictTo(req:Request,res:Response,next:NextFunction){
